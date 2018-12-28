25181abbdd8034ae039a47bdf27f212d5c826490
LilaElefnatTippfehler <andreas.wildmann@gmail.com>
2018-12-28 12:57:09 +0100

    pw

---
+++ src/main.cpp
@@ -0,0 +1,123 @@
+#include <ESP8266WiFi.h>
+#include "config.h"
+
+#define LED D1
+void printWifiStatus();
+String prepareHtmlPage();
+
+char ssid[] = "x x";      // your network SSID (name)
+char pass[] = "x";   // your network password
+
+WiFiServer server(80);
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+
+  WiFi.begin(ssid, pass);
+
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+
+  pinMode(LED, OUTPUT);
+
+  server.begin();
+
+   printWifiStatus();
+}
+
+void loop() {
+
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+
+  // Wait until the client sends some data
+  Serial.println("new client");
+  unsigned long timeout = millis() + 3000;
+  while (!client.available() && millis() < timeout) {
+    delay(1);
+  }
+  if (millis() > timeout) {
+    Serial.println("timeout");
+    client.flush();
+    client.stop();
+    return;
+  }
+
+  // Read the first line of the request
+  String req = client.readStringUntil('\r');
+  Serial.println(req);
+  client.flush();
+
+  // Match the request
+  int val;
+  if (req.indexOf("/gpio/0") != -1) {
+    val = 0;
+  } else if (req.indexOf("/gpio/1") != -1) {
+    val = 1;
+  } else {
+    Serial.println("invalid request");
+    client.print("HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html><body>Not found</body></html>");
+    return;
+  }
+
+  // Set GPIO2 according to the request
+  digitalWrite(LED, val);
+  client.flush();
+
+  // Prepare the response
+  String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\nGPIO is now ";
+  s += (val) ? "high" : "low";
+  s += "</html>\n";
+
+  // Send the response to the client
+  client.print(s);
+  delay(1);
+Serial.println("Client disconnected");
+}
+
+
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
+
+String prepareHtmlPage()
+{
+  String htmlPage =
+     String("HTTP/1.1 200 OK\r\n") +
+            "Content-Type: text/html\r\n" +
+            "Connection: close\r\n" +  // the connection will be closed after completion of the response
+            "Refresh: 5\r\n" +  // refresh the page automatically every 5 sec
+            "\r\n" +
+            "<!DOCTYPE HTML>" +
+            "<html>" +
+            "Analog input:  " + String(analogRead(A0)) +
+            "</html>" +
+            "\r\n";
+  return htmlPage;
+}
