d511af13dc6f0f754d4ad8c1c127db77ed3e8dec
LilaElefnatTippfehler <andreas.wildmann@gmail.com>
2018-12-28 12:41:42 +0100

    delete pwm support

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <ESP8266WiFi.h>
-#define LED D0
-#define PWM D1
+#define LED D1
 void printWifiStatus();
 String prepareHtmlPage();
 
@@ -63,24 +62,10 @@ void loop() {
 
   // Match the request
   int val;
-  int duty;
   if (req.indexOf("/gpio/0") != -1) {
     val = 0;
   } else if (req.indexOf("/gpio/1") != -1) {
     val = 1;
-  } else if(req.indexOf("/pwm/") != -1){
-    String num;
-    int i=0;
-    int len=req.length()-3;
-    while(i<len){
-      if(isDigit(req[i])){
-        num += req[i];
-      }
-      i++;
-    }
-    duty = num.toInt();
-    Serial.println(num);
-    Serial.println(duty);
   } else {
     Serial.println("invalid request");
     client.print("HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html><body>Not found</body></html>");
@@ -89,17 +74,11 @@ void loop() {
 
   // Set GPIO2 according to the request
   digitalWrite(LED, val);
-  analogWrite(PWM,duty);
-  int proz = duty/10;
-  String prozS = String(duty/10);
   client.flush();
 
   // Prepare the response
   String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\nGPIO is now ";
   s += (val) ? "high" : "low";
-  s += "\r\n\r\n PWM is set to ";
-  s += prozS;
-  s += "%";
   s += "</html>\n";
 
   // Send the response to the client
