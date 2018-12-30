9fb0f66dfa64851bcb2e0090289da09ea408879a
LilaElefnatTippfehler <andreas.wildmann@gmail.com>
2018-12-30 23:54:43 +0100

    HTML overhaul from root you can now turn the relais on or off, also you can access the time the relais was activated. No energy saving mode while it is accessed.

--- src/main.cpp
+++ src/main.cpp
@@ -14,13 +14,18 @@ void handleOn();
 void handleOff();
 void handleStatus();
 void handleRoot();
+void handleUpTime();
 String generateHTML();
+void updateOnTime();
+void changeState(int i);
 
 //WiFiServer server(8080);
 AdafruitIO_Feed *status = io.feed("on-slash-off");
 ESP8266HTTPUpdateServer httpUpdater;
 ESP8266WebServer httpServer(80);
 int PinStatus = 0;
+unsigned long onTime = 0;
+unsigned long lastCheck = 0;
 
 const char* host = "esp8266-webupdate";
 const char* update_path = "/firmware";
@@ -65,6 +70,7 @@ void setup()
         httpServer.on("/off",handleOff);
         httpServer.on("/status",handleStatus);
         httpServer.on("/",handleRoot);
+        httpServer.on("/time",handleUpTime);
         //ESP.deepSleep(uint64_t time_us, optional RFMode mode);
         wifi_set_sleep_type(MODEM_SLEEP_T);
 
@@ -106,16 +112,14 @@ void printWifiStatus() {
 }
 
 void handleOn(){
-        //httpServer.send(200, "text/plain", "Relais is turned on");
-        digitalWrite(LED, 1);
-        PinStatus = 1;
+        httpServer.send(200, "text/plain", "Relais is turned on");
+        changeState(1);
         status->save(DEVICE_NAME+'1');
 }
 
 void handleOff(){
-        //httpServer.send(200, "text/plain", "Relais is turned off");
-        digitalWrite(LED, 0);
-        PinStatus = 0;
+        httpServer.send(200, "text/plain", "Relais is turned off");
+        changeState(0);
         status->save(DEVICE_NAME+'0');
 }
 
@@ -146,6 +150,37 @@ void handleRoot() {
         httpServer.send(200, "text/html", generateHTML());
 }
 
+void handleUpTime(){
+        updateOnTime();
+        unsigned long sec = (millis()/1000%60);
+        unsigned long min = (millis()/1000/60%60);
+        unsigned long hr = (millis()/1000/60/60);
+        unsigned long Onsec = (onTime/1000%60);
+        unsigned long Onmin = (onTime/1000/60%60);
+        unsigned long Onhr = (onTime/1000/60/60);
+        char temp[400];
+        snprintf(temp, 400,
+
+                 "<html>\
+        <head>\
+          <meta http-equiv='refresh' content='1'/>\
+          <style>\
+            body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\
+          </style>\
+        </head>\
+        <body>\
+          <p>Uptime: %02ld:%02ld:%02ld</p>\
+          <p>Ontime: %02ld:%02ld:%02ld</p>\
+        </body>\
+      </html>",
+
+                 hr, min, sec,Onhr, Onmin, Onsec
+                 );
+        httpServer.send(200, "text/html", temp);
+}
+
+
+
 void handleMessage(AdafruitIO_Data *data){
         Serial.print("received <- ");
         Serial.println(data->value());
@@ -153,43 +188,57 @@ void handleMessage(AdafruitIO_Data *data){
         String nameOn = DEVICE_NAME;
         String nameOff = DEVICE_NAME;
         if(!data_rec.compareTo(nameOff+ '0')) {
-                digitalWrite(LED, 0);
-                PinStatus = 0;
+                changeState(0);
                 Serial.println("turning Off");
         }
         if(!data_rec.compareTo(nameOn+ '1')) {
-                digitalWrite(LED, 1);
-                PinStatus = 1;
+                changeState(1);
                 Serial.println("turning On");
         }
         if(!data_rec.compareTo("ein")) {
-                digitalWrite(LED, 1);
-                PinStatus = 1;
+                changeState(1);
                 Serial.println("turning On");
         }
         if(!data_rec.compareTo("aus")) {
-                digitalWrite(LED, 0);
-                PinStatus = 0;
-                Serial.println("turning On");
+                changeState(0);
+                Serial.println("turning off");
+        }
+}
+
+void updateOnTime(){
+        if(lastCheck != 0) {
+                onTime += millis() - lastCheck;
+                lastCheck = millis();
+        }
+}
+
+void changeState(int i){
+        if(PinStatus != i) {
+                if(lastCheck != 0) {
+                        updateOnTime();
+                        lastCheck = 0;
+                }else{
+                        lastCheck = millis();
+                }
+                digitalWrite(LED, i);
+                PinStatus = i;
         }
 }
 
 String generateHTML(){
-        String sec = String(millis()/1000,DEC);
-        String min = String(millis()/1000/60,DEC);
-        String hr = String(millis()/1000/60/60,DEC);
+
         return "<html>\
 <head>\
-<meta http-equiv='refresh' content='5'/>\
 <title>Smart Relais</title>\
 <style>\
 body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\
 </style>\
 </head>\
 <body>\
-<iframe width='0' height='0' border='0' name='dummyframe' id='dummyframe'></iframe>\
 <h1>Smart Relais</h1>\
-<p>Uptime: " + hr + ":" + min + ":" + sec + "</p>\
+<iframe src='/time' width='250' height='80' border='0' name='uptime' id='uptime'></iframe>\
+<br>\
+<iframe src='/status' width='250' height='30' border='0' name='dummyframe' id='dummyframe'></iframe>\
 <form action='/on' target='dummyframe' method='post'>\
 <input type='submit' value='Turn on'>\
 </form>\
